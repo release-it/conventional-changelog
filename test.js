a6a2ef5dffeac4304e4fffbbe0737b9376dd8bfe
Lars Kappert <lars@webpro.nl>
2019-06-06 20:12:20 +0200

    Replace ava with bron

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const test = require('ava');
+const test = require('bron');
+const assert = require('assert');
 const { EOL } = require('os');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
@@ -31,16 +32,10 @@ const namespace = 'conventional-changelog';
 const preset = 'angular';
 const infile = 'CHANGES.md';
 
-test.after.always(() => {
-  try {
-    fs.unlinkSync(infile);
-  } catch (err) {}
-});
-
 test('should not throw', async t => {
   const options = { [namespace]: { preset } };
   const plugin = factory(Plugin, { namespace, options });
-  await t.notThrowsAsync(runTasks(plugin));
+  await assert.doesNotReject(runTasks(plugin));
 });
 
 test('should set changelog', async t => {
@@ -48,7 +43,7 @@ test('should set changelog', async t => {
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
   const { changelog } = plugin.config.getContext();
-  t.is(changelog, 'The changelog');
+  assert.strictEqual(changelog, 'The changelog');
 });
 
 test('should use recommended bump', async t => {
@@ -56,7 +51,7 @@ test('should use recommended bump', async t => {
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
   const { version } = plugin.config.getContext();
-  t.is(version, '1.1.0');
+  assert.strictEqual(version, '1.1.0');
 });
 
 test('should use provided increment', async t => {
@@ -64,7 +59,7 @@ test('should use provided increment', async t => {
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
   const { version } = plugin.config.getContext();
-  t.is(version, '2.0.0');
+  assert.strictEqual(version, '2.0.0');
 });
 
 test('should use provided version', async t => {
@@ -72,7 +67,7 @@ test('should use provided version', async t => {
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
   const { version } = plugin.config.getContext();
-  t.is(version, '1.2.3');
+  assert.strictEqual(version, '1.2.3');
 });
 
 test(`should write and update infile (${infile})`, async t => {
@@ -80,24 +75,25 @@ test(`should write and update infile (${infile})`, async t => {
   const plugin = factory(Plugin, { namespace, options });
   await runTasks(plugin);
   const changelog = fs.readFileSync(infile);
-  t.is(changelog.toString().trim(), 'The changelog');
+  assert.strictEqual(changelog.toString().trim(), 'The changelog');
   {
     await runTasks(plugin);
     const changelog = fs.readFileSync(infile);
-    t.is(changelog.toString().trim(), `The changelog${EOL}${EOL}The changelog`);
+    assert.strictEqual(changelog.toString().trim(), `The changelog${EOL}${EOL}The changelog`);
   }
+  fs.unlinkSync(infile);
 });
 
 test('should reject if conventional bump passes error', async t => {
   const options = { [namespace]: { preset: 'what?' } };
   const plugin = factory(Plugin, { namespace, options });
-  await t.throwsAsync(runTasks(plugin), /Something went wrong/);
+  await assert.rejects(runTasks(plugin), /Something went wrong/);
 });
 
 test('should reject if conventional changelog has error', async t => {
   const options = { [namespace]: { preset, releaseCount: -1 } };
   const plugin = factory(Plugin, { namespace, options });
-  await t.throwsAsync(runTasks(plugin), /Something went wrong/);
+  await assert.rejects(runTasks(plugin), /Something went wrong/);
 });
 
 test('should not write infile in dry run', async t => {
@@ -106,6 +102,6 @@ test('should not write infile in dry run', async t => {
   const plugin = factory(Plugin, { namespace, options, global: { isDryRun: true } });
   const spy = sinon.spy(plugin, 'writeChangelog');
   await runTasks(plugin);
-  t.is(spy.callCount, 0);
-  t.throws(() => fs.readFileSync(infile), /no such file/);
+  assert.strictEqual(spy.callCount, 0);
+  assert.throws(() => fs.readFileSync(infile), /no such file/);
 });
