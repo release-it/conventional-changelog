1b1c83dc0f23e848e1dd2a70ce585c47980ec663
Lars Kappert <lars@webpro.nl>
2022-01-09 12:14:24 +0100

    Fix release type for pre-release

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ class ConventionalChangelog extends Plugin {
         if (increment && semver.valid(increment)) {
           resolve(increment);
         } else if (isPreRelease) {
-          const type = increment ? `pre${releaseType}` : 'prerelease';
+          const type = releaseType ? `pre${releaseType}` : 'prerelease';
           resolve(semver.inc(latestVersion, type, preReleaseId));
         } else if (releaseType) {
           resolve(semver.inc(latestVersion, releaseType, preReleaseId));
