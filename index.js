4234126514c426f208209d2d0019447a7081e101
Felix Winterhalter <felix@ranovus.com>
2024-06-07 16:15:58 +0200

    fix: Fix conventional changelog misbehaving in prerelease mode

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import conventionalRecommendedBump from 'conventional-recommended-bump';
 import conventionalChangelog from 'conventional-changelog';
 import semver from 'semver';
 import concat from 'concat-stream';
+import gitSemverTags from 'git-semver-tags';
 
 class ConventionalChangelog extends Plugin {
   static disablePlugin(options) {
@@ -46,6 +47,45 @@ class ConventionalChangelog extends Plugin {
       if (increment && semver.valid(increment)) {
         return increment;
       } else if (isPreRelease) {
+
+        // Determine what our diff to the last non-prerelease would be
+        const { releaseType: releaseTypeToLastNonPrerelease } = await conventionalRecommendedBump({...options, skipUnstable: true});
+
+        // Get the latest non prerelease tag
+        const semVerTags = await gitSemverTags({
+          lernaTags: !!options.lernaPackage,
+          package: options.lernaPackage,
+          tagPrefix: options.tagPrefix,
+          skipUnstable: true,
+          cwd: options.cwd
+        });
+
+        // Make sure we have at least one tag else we can completely ignore this
+        if(semVerTags.length > 0) {
+          const lastTag = semVerTags[0]
+
+          // If the difference to the last non prerelease should be major but our major numbers are the same do a premajor
+          if (releaseTypeToLastNonPrerelease == "major") {
+              if(semver.major(lastTag) == semver.major(latestVersion)) {
+                return semver.inc(latestVersion, "premajor", preReleaseId);
+              }
+          }
+          // Same as above for minor
+          if (releaseTypeToLastNonPrerelease == "minor") {
+            if(semver.minor(lastTag) == semver.minor(latestVersion)) {
+              return semver.inc(latestVersion, "preminor", preReleaseId);
+            }
+          }    
+          // Same as above for patch
+          if (releaseTypeToLastNonPrerelease == "patch") {
+            if(semver.patch(lastTag) == semver.patch(latestVersion)) {
+              return semver.inc(latestVersion, "prepatch", preReleaseId);
+            }
+          }
+
+          // If none of the above match, then we are at the right version and just need to do a prerelease
+          return semver.inc(latestVersion, "prerelease", preReleaseId);    
+        }
         const type =
           releaseType && (options.strictSemVer || !semver.prerelease(latestVersion))
             ? `pre${releaseType}`
